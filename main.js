573b37dd414194c01fa9f45d0eb2f61bf58c3edc
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-07-06 16:07:54 +0200

    test

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ try {
 } catch(ignore) {}
 
 const promises = [
-  new Promise((resolve, reject) => {
+  new Promise(resolve => {
     if(config.id) return resolve(config.id);
     si.system((data) => {
       resolve(data);
@@ -60,57 +60,33 @@ Promise.all(promises).then(values => {
     cpus: []
   };
 
-  for (let index = 0; index < cpu.length; ++index) {
-    const line = cpu[index];
-    if(!line.startsWith('cpu')) break;
-    const splittedLine = line.replace(/ {2}/g, ' ').split(/ /g);
-
-    /*
-     *
-     user: normal processes executing in user mode
-     nice: niced processes executing in user mode
-     system: processes executing in kernel mode
-     idle: twiddling thumbs
-     iowait: waiting for I/O to complete
-     irq: servicing interrupts
-     softirq: servicing softirqs
-     * */
-
-    const result = {
-      cpuName: splittedLine[0],
-      user: splittedLine[1],
-      nice: splittedLine[2],
-      system: splittedLine[3],
-      idle: splittedLine[4],
-      iowait: splittedLine[5],
-      irq: splittedLine[6],
-      softirq: splittedLine[7],
-      steal: splittedLine[8],
-      guest: splittedLine[9],
-      guest_nice: splittedLine[10]
-    };
+  cpu.some((line, index) => {
+    if(!line.startsWith('cpu')) return true;
+    const [cpuName, user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice] = line.replace(/\s+/, ' ').split(/ /g);
+    const result = { cpuName, user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice };
 
     if(index === 0) {
       cpuResult.avg = result;
     } else {
-      cpuResult.cpus[index - 1] = result;
+      cpuResult.cpus.push(result);
     }
-  }
+  });
 
   const diskResult = [];
 
-  for(let index = 1; index < disk.length; ++index) {
-    const line = disk[index].split(/\s+/);
-    if(line.length < 6) {
-      continue;
-    }
-    diskResult[index - 1] = {
+  disk.forEach((line, index) => {
+    if (index === 0) return;
+    line = line.split(/\s+/);
+    if(line.length < 6) return;
+
+    diskResult.push({
       name: line[0],
       capacity: line[5],
       used: line[2],
       available: line[3]
-    };
-  }
+    });
+
+  });
 
   const out = {
     id: argv.id || config.id || values[0], //todo aggiungi caricato da file di ubuntu,
@@ -127,7 +103,7 @@ Promise.all(promises).then(values => {
 
   console.log(JSON.stringify(out));
 
-  /*const s3 = initializeS3(config, argv);
+  /*const s3 = _initializeS3(config, argv);
 
   s3.upload({
     Bucket: 'bucket',
@@ -138,7 +114,7 @@ Promise.all(promises).then(values => {
 });
 
 
-function initializeS3(config, argv) {
+function _initializeS3(config, argv) {
   if(config.aws) {
     return new AWS.S3(new AWS.config(config.aws));
   } else if(accessKeyId) {
