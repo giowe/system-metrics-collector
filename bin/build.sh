19700b13b0e9e8aecf6d1fbe30b0c1f96b8fc76a
MatteCarra <matte.carra@gmail.com>
2017-07-07 15:35:44 +0200

    build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -14,4 +14,6 @@ if [ $ret_code_nexe != 0 ]; then
   echo "Nexe compilation failed!"
   exit $ret_code_nexe
 fi
-echo "You can find the compiled file in ./out/sfcw_linux_${MACHINE_TYPE}"
+FILE_NAME="${PWD}/out/sfcw_linux_${MACHINE_TYPE}"
+eval "mv ${PWD}/out/sfcw ${FILE_NAME}"
+echo "You can find the compiled file in ${FILE_NAME}"
