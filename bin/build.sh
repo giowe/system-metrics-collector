c0121c7dcf2ea67fe6b6141adf9748bcccadc9d9
MatteCarra <matte.carra@gmail.com>
2017-07-07 12:14:03 +0200

    Updated script

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,16 @@
 
 MACHINE_TYPE=`uname -m`
 eval "npm install"
+ret_code=$?
+if [ $ret_code != 0 ]; then
+  echo "Npm install failed!"
+  exit $ret_code
+fi
+eval "mkdir -p ${PWD}/..out"
 eval "nexe -i ${PWD}/../main.js -o ${PWD}/../out/sfcw_linux_${MACHINE_TYPE} --bundle"
+ret_code_nexe=$?
+if [ $ret_code_nexe != 0 ]; then
+  echo "Nexe compilation failed!"
+  exit $ret_code_nexe
+fi
 echo "You can find the compiled file in ./out/sfcw_linux_${MACHINE_TYPE}"
