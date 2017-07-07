b1ec6fca90f7d73096495b9f7b1d2a6da6ad35ca
MatteCarra <matte.carra@gmail.com>
2017-07-07 12:05:54 +0200

    Update

--- bin/build.sh
+++ bin/build.sh
@@ -3,3 +3,4 @@
 MACHINE_TYPE=`uname -m`
 eval "npm install"
 eval "nexe -i ${PWD}/../main.js -o ${PWD}/../out/sfcw_linux_${MACHINE_TYPE} --bundle"
+echo "You can find the compiled file in ./out/sfcw_linux_${MACHINE_TYPE}"
